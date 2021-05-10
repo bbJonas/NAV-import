df11558572c03f0e5594c5054fd3763ca19bebe3
bbJonas <43041467+bbJonas@users.noreply.github.com>
2021-05-10 20:19:08 +0200

    add import folder scan, parse xlsx to json

--- app.js
+++ app.js
@@ -2,27 +2,35 @@
 const path = require('path');
 const fs = require('fs');
 // required external librarys
-const xlsx = require('node-xlsx');
+const xlsx = require('node-xlsx').default;
 
-// //  excel workbook
-// const wb = xlsx.parse(`${__dirname}/myFile.xlsx`)
-//
-// // target worksheet
-// const ws = ();
 
 //  find working directory
 var appDirectory = path.parse(__filename).dir.normalize();
-//set importFolder
+//set importFolder and array
 var importFolder = `${appDirectory}\\import`;
+var importArray = [];
+
 //look for sheets in importFolder
 fs.readdir(importFolder, (err, files) => {
   if (err)
     console.log(err);
   else {
-    console.log("\Filenames with the .txt extension:");
+    console.log("\Filenames with the .xlsx extension:");
     files.forEach(file => {
       if (path.extname(file) == ".xlsx")
         console.log(file);
+        const wbBuffer = xlsx.parse(fs.readFileSync(`${importFolder}/${file}`,{cellDates:true}));
+        console.log(wbBuffer);
     })
   }
 })
+
+importArray.forEach(file => {
+  //  excel workbook
+  const wbBuffer = xlsx.parse(fs.readFileSync(`${importFolder}/${file}`));
+  // target worksheet
+  const ws = wbBuffer.Sheets["Holdingsreport"];
+  var data = xlsx.utils.sheet_to_json(ws);
+  console.log(data);
+});

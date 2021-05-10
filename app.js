ee6588d289f89ad84f3c7880dfedba05a0eb3643
bbJonas <43041467+bbJonas@users.noreply.github.com>
2021-05-11 00:22:07 +0200

    import and manipulate data

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const fs = require('fs');
 // required external librarys
 const xlsx = require('node-xlsx').default;
 
+// Tracked Crypto and FIAT
+const crypto = [
+  {name: "btc", symbol: 'BITCOINS/USD'},
+  {name: "eth", symbol: 'ETHEREUM/USD'},
+  {name: "bch", symbol: 'Bitcoin Cash/USD'},
+  {name: "ada", symbol: 'Cardano/USD -ADA-'},
+  {name: "dash", symbol: 'Dash/USD'}
+];
+const fiat = [
+  {name: "eur", symbol: 'KK EUR'},
+  {name: "usd", symbol: 'KK USD'}
+];
 
 //  find working directory
 var appDirectory = path.parse(__filename).dir.normalize();
@@ -21,16 +33,24 @@ fs.readdir(importFolder, (err, files) => {
       if (path.extname(file) == ".xlsx")
         console.log(file);
         const wbBuffer = xlsx.parse(fs.readFileSync(`${importFolder}/${file}`,{cellDates:true}));
-        console.log(wbBuffer);
+        const ws = wbBuffer[0].data;
+        const flat = ws.flat(2);
+        const filtered = flat.filter(Boolean);
+        crypto.forEach((coin, i) => {
+          index = filtered.findIndex(x => x = coin.symbol.toString());
+          console.log(coin.symbol);
+          console.log(index);
+        });
+
     })
   }
 })
 
-importArray.forEach(file => {
-  //  excel workbook
-  const wbBuffer = xlsx.parse(fs.readFileSync(`${importFolder}/${file}`));
-  // target worksheet
-  const ws = wbBuffer.Sheets["Holdingsreport"];
-  var data = xlsx.utils.sheet_to_json(ws);
-  console.log(data);
-});
+// importArray.forEach(file => {
+//   //  excel workbook
+//   const wbBuffer = xlsx.parse(fs.readFileSync(`${importFolder}/${file}`));
+//   // target worksheet
+//   const ws = wbBuffer.Sheets["Holdingsreport"];
+//   var data = xlsx.utils.sheet_to_json(ws);
+//   console.log(data);
+// });
